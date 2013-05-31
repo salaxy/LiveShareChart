f0574c59cc5605ce4502082eb6ea52451656405f
Frank Mertens <mertens@fh-brandenburg.de>
2013-05-31 14:47:42 +0200

    Chart zeigt jetzt den Kurs aus der DB an

--- src/de/fhb/trendsys/lsc/db/control/BusinessLogic.java
+++ src/de/fhb/trendsys/lsc/db/control/BusinessLogic.java
@@ -2,6 +2,10 @@ package de.fhb.trendsys.lsc.db.control;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javafx.scene.chart.XYChart;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
@@ -41,8 +45,23 @@ public class BusinessLogic {
 		}
 		
 		public void run() {
-			List<Map<String, AttributeValue>> item = ddbClient.getAllItems(id);
-			
+			List<Map<String, AttributeValue>> itemList = ddbClient.getAllItems(id);
+			for (Map<String, AttributeValue> itemMap : itemList) {
+				Set<Entry<String, AttributeValue>>  itemSet = itemMap.entrySet();
+				
+				String timeStamp = null;
+				Double stockValue = 0d;
+				
+				for (Entry<String, AttributeValue> item : itemSet) {
+					if (timeStamp == null)
+						timeStamp = item.getValue().getS();
+					else
+						stockValue = Double.parseDouble(item.getValue().getS());
+				}
+				
+				model.getDataSeries().getData().add(new XYChart.Data<String, Number>(timeStamp, stockValue));
+			}
+				
 		}
 	}
 
