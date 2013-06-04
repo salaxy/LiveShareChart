2ab5d436d05bfebc9bb5b38d0976363d09c95e55
Frank Mertens <mertens@fh-brandenburg.de>
2013-06-04 18:17:04 +0200

    Grabber fragt jetzt aus RSS-Feeds ab.
    Er verwendet dazu JavaXt und speichert von neuen News den Titel, die
    Beschreibung und den Link zur eigentlichen News ab.

--- src/de/fhb/trendsys/stockgrabber/Grabber.java
+++ src/de/fhb/trendsys/stockgrabber/Grabber.java
@@ -1,7 +1,16 @@
 package de.fhb.trendsys.stockgrabber;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import javaxt.http.Request;
+import javaxt.rss.Feed;
+import javaxt.rss.Item;
+import javaxt.rss.Parser;
+
+import org.w3c.dom.Document;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
@@ -9,31 +18,60 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import de.fhb.trendsys.amazonfunctions.dynamodb.DynamoDBHandler;
 
 public class Grabber {
+	public static final long delayChartUpdate = 60000L; // 1 Min.
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		DynamoDBHandler ddbClient = new DynamoDBHandler(Regions.EU_WEST_1, "stockdata");
 		
+		String lastNewsTitle = "";
+		long id = 1;
+		URL rssUrl = new URL("http://www.sony.de/rss/de_DE/All.rss");
+		String stockUrl = "http://www.sony.net";
+		
 		while (true) {
+			Long currentTime = System.currentTimeMillis();
+			
+			// DB-Item generieren
 			Map<String, AttributeValue> stockDataItem = new HashMap<String, AttributeValue>();
-			stockDataItem.put("id", new AttributeValue().withN("1"));
-			stockDataItem.put("timestamp", new AttributeValue().withS(Long.toString(System.currentTimeMillis())));
+			stockDataItem.put("id", new AttributeValue().withN(Long.toString(id)));
+			stockDataItem.put("timestamp", new AttributeValue().withS(Long.toString(currentTime)));
 			stockDataItem.put("stock", new AttributeValue().withS(Double.toString(Math.random())));			
 			
-			if (System.currentTimeMillis() % 10 == 0) {
-				stockDataItem.put("stock", new AttributeValue().withS(Double.toString(Math.random())));
+			// News-Feed abholen			
+			Document feedXml = new Request(rssUrl).getResponse().getXML();
+			Feed[] feeds = new Parser(feedXml).getFeeds();
+			
+			if (!feeds[0].getItems()[0].getTitle().equals(lastNewsTitle)) {
+				Item item = feeds[0].getItems()[0];
+				String feedTitle = item.getTitle();
+				String feedDescription = item.getDescription();
+				String feedUrl = item.getLink().toExternalForm();
 				
-				int numberOfNews = (int) (Math.random() * 2d);
-				for (int i = 0; i < numberOfNews; i++) {
-					stockDataItem.put("news" + i, new AttributeValue().withS("Nachricht " + i));
-				}
+				if (!feedTitle.isEmpty())
+					stockDataItem.put("newstitle", new AttributeValue().withS(feedTitle));
+				else
+					stockDataItem.put("newstitle", new AttributeValue().withS("Neue Nachrichten"));
+					
+				if (!feedDescription.isEmpty())
+					stockDataItem.put("newsdescription", new AttributeValue().withS(feedDescription));
+				else
+					stockDataItem.put("newsdescription", new AttributeValue().withS("keine Beschreibung"));
+				
+				if (!feedUrl.isEmpty())
+					stockDataItem.put("newsurl", new AttributeValue().withS(feedUrl));
+				else
+					stockDataItem.put("newsurl", new AttributeValue().withS(stockUrl));
+				
+				lastNewsTitle = feedTitle;
 			}
-				
+
 			ddbClient.addItem(1, stockDataItem);
 			
 			System.out.println("Item: " + stockDataItem.toString());
+			System.out.println("Last news title: " + lastNewsTitle);
 			
 			try {
-				Thread.sleep(500);
+				Thread.sleep(delayChartUpdate);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
